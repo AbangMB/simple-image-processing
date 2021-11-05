68b42a953f840192aa323af01fc22fc442f0a954
nasipadank <92131184+nasipadank@users.noreply.github.com>
2021-11-05 07:39:07 +0700

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/*
+    Judul : Operasi-Operasi Dasar Citra dan Pendeteksian Tepi (Edge Detection)
+    Bahasa : Java
+    Kelas : TI 5 Reguler C
+    Anggota :
+        - Josie G.F Purba (09021281924044)
+        - M. Friza Dwi Aditya Frinison (09021181924018)
+    https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/
+*/
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
