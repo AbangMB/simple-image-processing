02001c777f3c90050be7bd192630116d17e0edab
nasipadank <92131184+nasipadank@users.noreply.github.com>
2021-11-09 08:34:56 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,8 +5,10 @@
     Anggota :
         - Josie G.F Purba (09021281924044)
         - M. Friza Dwi Aditya Frinison (09021181924018)
+        - Abang Muhammad Bagus (09021281924044)
     https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/
 */
+
 public class App {
 
     public static void main(String[] args) throws Exception {
